dda404ad5ba327f0896858fc84a7db7f58db168a
Carolina Alves <carol.alves99@live.com>
2020-01-01 16:19:14 -0300

    Ajustando a deleção de clientes

--- src/main/java/com/carolinaalves/cursospringboot/services/ClienteService.java
+++ src/main/java/com/carolinaalves/cursospringboot/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
     try {
       repository.deleteById(id);
     } catch(DataIntegrityViolationException e) {
-      throw new DataIntegrityException("Não é possível excluir um cliente que possui vínculos");
+      throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos vinculados");
     }
   }
 
