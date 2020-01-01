a975ddf3c705780f31c3162395f54935a3dfad88
Carolina Alves <carol.alves99@live.com>
2020-01-01 10:22:38 -0300

    Criado endpoint de deleção de categoria

--- src/main/java/com/carolinaalves/cursospringboot/services/CategoriaService.java
+++ src/main/java/com/carolinaalves/cursospringboot/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.carolinaalves.cursospringboot.services;
 
 import com.carolinaalves.cursospringboot.domain.Categoria;
 import com.carolinaalves.cursospringboot.repository.CategoriaRepository;
+import com.carolinaalves.cursospringboot.services.exceptions.DataIntegrityException;
 import com.carolinaalves.cursospringboot.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -27,5 +29,14 @@ public class CategoriaService {
   public Categoria update(Categoria categoria) {
     find(categoria.getId());
     return repository.save(categoria);
+  }
+
+  public void delete(Integer id) {
+    find(id);
+    try {
+      repository.deleteById(id);
+    } catch(DataIntegrityViolationException e) {
+      throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos vinculados");
+    }
   }
 }
---
+++ src/main/java/com/carolinaalves/cursospringboot/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,11 @@
+package com.carolinaalves.cursospringboot.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException {
+  public DataIntegrityException(String message) {
+    super(message);
+  }
+
+  public DataIntegrityException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
